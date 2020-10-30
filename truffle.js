2fe65de9e9de46e328a7f3f388535034bb42f8d8
kasper.keunen <kasper@gutstickets.org>
2020-10-30 10:41:27 +0100

    added Klaytn config

--- truffle.js
+++ truffle.js
@@ -10,7 +10,7 @@ module.exports = {
         return new HDWalletProvider(MNEMONIC, "https://ropsten.infura.io/v3/xxx")
       },
       network_id: 3,
-      gas: 39368     //make sure this gas allocation isn't over 4M, which is the max
+      gas: 39368     // make sure this gas allocation isn't over 4M, which is the max
     },  
     ganache: {
       host: "127.0.0.1",
@@ -19,13 +19,20 @@ module.exports = {
       from: "XXX",
       gas: 6621975
     },
-    bsctestnet: {
+    bsctestnet: { // binance chain integraton (testnet)
       provider: function() {
         return new HDWalletProvider(MNEMONIC, "https://data-seed-prebsc-1-s1.binance.org:8545/")
       },
       network_id: 97,
       gas: 30000000,
-    }    
+    },
+    klaytntestnet: { // Klaytn chain integraton (testnet)
+      provider: function() {
+        return new HDWalletProvider(MNEMONIC, "xxx/")
+      },
+      network_id: 97,
+      gas: 30000000,
+    }        
   },
   compilers: { 
     solc: {
